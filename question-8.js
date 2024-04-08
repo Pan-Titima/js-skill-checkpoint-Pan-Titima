b4e1421008b947700b2de25a3195fc9e4f12049a
Titima Kasemsritanawat <titima.kp@gmail.com>
2024-04-08 15:56:14 +0700

    Answer for question-8.js

--- question-8.js
+++ question-8.js
@@ -1,3 +1,21 @@
 // Question #8: Fetching User List from Server
 
 // เริ่มเขียนโค้ดตรงนี้
+
+const getUsers = async () => {
+  return fetch("https://jsonplaceholder.typicode.com/users")
+            .then(response => response.json())
+            .then(data => {return data.map(x => x.name)});
+
+}
+
+async function asynconousFunction(){
+  const newUsers = await getUsers();
+  console.log(newUsers);
+}
+
+asynconousFunction();
+
+
+
+
